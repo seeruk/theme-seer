af1cea248393fa9910aa73efb5ab9008a919a094
Elliot Wright <wright.elliot@gmail.com>
2017-02-01 19:20:08 +0000

    Changed how variables are declared to make it work without re-opening the theme. Dropped terminator to new line. Experimenting...

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,25 +1,25 @@
 # Colours
-set __seer_trivial_color        (set_color brgrey)
-set __seer_normal_color         (set_color normal)
-set __seer_success_color        (set_color cyan)
-set __seer_error_color          (set_color red)
-set __seer_directory_color      (set_color white)
-set __seer_bold_directory_color (set_color white --bold)
-set __seer_pristine_repo_color  (set_color green)
-set __seer_touched_repo_color   (set_color yellow)
-set __seer_git_directory_color  (set_color magenta)
+set -gu __seer_trivial_color        (set_color brgrey)
+set -gu __seer_normal_color         (set_color normal)
+set -gu __seer_success_color        (set_color cyan)
+set -gu __seer_error_color          (set_color red)
+set -gu __seer_directory_color      (set_color white)
+set -gu __seer_bold_directory_color (set_color white --bold)
+set -gu __seer_pristine_repo_color  (set_color green)
+set -gu __seer_touched_repo_color   (set_color yellow)
+set -gu __seer_git_directory_color  (set_color purple)
 
 # Symbols
-set __seer_alive_whale ". ><((.___)"
-set __seer_dead_whale  ". ><((x___)"
-set __seer_ahead       " ↑"
-set __seer_behind      " ↓"
-set __seer_diverged    " ↕"
-set __seer_dirty       " ✘"
-set __seer_none        " ≈"
+set -gu __seer_alive_whale ". ><((.___)"
+set -gu __seer_dead_whale  ". ><((x___)"
+set -gu __seer_ahead       " ↑"
+set -gu __seer_behind      " ↓"
+set -gu __seer_diverged    " ↕"
+set -gu __seer_dirty       " ✘"
+set -gu __seer_none        " ≈"
 
 # Helpers
-function seer_prompt_status -d "Display the whale, showing last command status"
+function __seer_prompt_status -d "Display the whale, showing last command status"
   if test $argv[1] -eq 0
     echo -n -s $__seer_trivial_color $__seer_alive_whale $__seer_normal_color
   else
@@ -27,11 +27,11 @@ function seer_prompt_status -d "Display the whale, showing last command status"
   end
 end
 
-function seer_path_parent -d "Display a parent directory, shortened to fit the prompt"
+function __seer_path_parent -d "Display a parent directory, shortened to fit the prompt"
   echo -n (dirname $argv[1]) | sed -e "s#^$HOME#~#" -e 's#/\(\.\{0,1\}[^/]\)\([^/]*\)#/\1#g' -e 's#/$##'
 end
 
-function seer_path_segment -d "Display a shortened form of a directory"
+function __seer_path_segment -d "Display a shortened form of a directory"
   set -l directory
   set -l parent
 
@@ -41,7 +41,7 @@ function seer_path_segment -d "Display a shortened form of a directory"
     case "$HOME"
       set directory "~"
     case "*"
-      set parent (seer_path_parent "$argv[1]")
+      set parent (__seer_path_parent "$argv[1]")
       set parent "$parent/"
       set directory (basename "$argv[1]")
   end
@@ -50,10 +50,10 @@ function seer_path_segment -d "Display a shortened form of a directory"
   echo -n -s $__seer_bold_directory_color $directory $__seer_normal_color
 end
 
-function seer_prompt_git -d "Display the git root, git branch, and then path in the repo"
+function __seer_prompt_git -d "Display the git root, git branch, and then path in the repo"
   set -l repo_root (command git rev-parse --show-toplevel ^/dev/null)
 
-  seer_path_segment $repo_root
+  __seer_path_segment $repo_root
 
   if git_is_touched
     echo -n -s " on " $__seer_touched_repo_color (git_branch_name) $__seer_normal_color
@@ -74,15 +74,17 @@ function seer_prompt_git -d "Display the git root, git branch, and then path in 
   end
 end
 
-function seer_prompt_dir -d "Display the entire path (but shortened)"
-  seer_path_segment (pwd)
+function __seer_prompt_dir -d "Display the entire path (but shortened)"
+  __seer_path_segment (pwd)
 end
 
-function seer_prompt_terminator -d "Shows the end of the prompt, before text, indicating root"
+function __seer_prompt_terminator -d "Shows the end of the prompt, before text, indicating root"
+  echo ""
+
   if [ (whoami) = "root" ]
-    echo -n -s $__seer_trivial_color " # " $__seer_normal_color
+    echo -n -s $__seer_trivial_color "⤷ # " $__seer_normal_color
   else
-    echo -n -s $__seer_trivial_color " \$ " $__seer_normal_color
+    echo -n -s $__seer_trivial_color "⤷ \$ " $__seer_normal_color
   end
 end
 
@@ -91,13 +93,13 @@ function fish_prompt
   set last_command_status $status
   set -l cwd (prompt_pwd)
 
-  seer_prompt_status $last_command_status
+  __seer_prompt_status $last_command_status
 
   if git_is_repo
-    seer_prompt_git
+    __seer_prompt_git
   else
-    seer_prompt_dir
+    __seer_prompt_dir
   end
 
-  seer_prompt_terminator
+  __seer_prompt_terminator
 end
