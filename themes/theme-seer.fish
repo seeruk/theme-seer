a4de57d4675213216aaf6a9bc442fea5f3e9dfd9
Elliot Wright <elliot.wright@icetravelgroup.com>
2024-03-13 20:54:59 +0000

    Moving around to support updated theme structure, maybe?

---
+++ themes/theme-seer.fish
@@ -0,0 +1,28 @@
+fish_color_autosuggestion 6B6F7D
+fish_color_cancel -r
+fish_color_command 4576FF
+fish_color_comment 6B6F7D
+fish_color_cwd green
+fish_color_cwd_root red
+fish_color_end A04FFF
+fish_color_error EF6155
+fish_color_escape 'bryellow'  '--bold'
+fish_color_history_current --bold
+fish_color_host normal
+fish_color_host_remote yellow
+fish_color_match --background=brblue
+fish_color_normal normal
+fish_color_operator bryellow
+fish_color_param 5BC4BF
+fish_color_quote 48B667
+fish_color_redirection brblue
+fish_color_search_match 'bryellow'  '--background=brblack'
+fish_color_selection 'white'  '--bold'  '--background=brblack'
+fish_color_status red
+fish_color_user brgreen
+fish_color_valid_path --underline
+fish_pager_color_completion
+fish_pager_color_description 'B3A06D'  'yellow'
+fish_pager_color_prefix 'white'  '--bold'  '--underline'
+fish_pager_color_progress 'brwhite'  '--background=cyan'
+fish_pager_color_selected_background -r
